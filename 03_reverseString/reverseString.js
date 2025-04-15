4e994cb66bdca9433c23a8b1bfc68efd1f76284f
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-14 19:40:03 -0700

    write pseudocode for reverseString() function
    this saves mistakes and pre-empts bugs

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
 const reverseString = function() {
+    //initialize result string
+    //initialize array
+    //initialize reversed array
+    //SPLIT string into array
+    //initialize for loop, loop equal to length of split array
+        //each iteration, add next letter of array to the FRONT of reversed array
+    //JOIN reversed array into string
+    //assign result to reversed string
 
 };
 
