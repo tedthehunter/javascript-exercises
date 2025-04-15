8f8e5867096c91b2bef177379690edf132e68c92
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-14 19:53:33 -0700

    write pseudocode for removeFromArray() logic
    this helps clarify and pre-empts bugs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+    //LOOK up how to handle multiple optional arguments
+    //look up how to modify array in place
+    //take input array and remove any instances of other arguments
+
+    //Initialize array called "removals"
+    //initialize for loop - iterate for each index in inputArray
+    //for each character in array, if removals.includes(character), remove from inputArray
+    //return modified array
 };
 
 // Do not edit below this line
