ebc968ab0eff08b90c5daa61981f88da1f34c3f4
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-14 19:12:30 -0700

    write pseudocode for repeatString() function
    plans out logic and pre-empts bugs and faulty logic

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    //Initialize result variable
+    //Start FOR loop, loop num times
+        //each loop, concat string to result
+    //return result
 };
 
 // Do not edit below this line
