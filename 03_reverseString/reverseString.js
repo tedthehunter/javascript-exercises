966c354d4bceacbae8d271c932322b40d4394984
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-14 19:43:08 -0700

    initialize all variables and write return statement
    this leaves only the more complex logic to work on

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,19 @@
-const reverseString = function() {
-    //initialize result string
-    //initialize array
-    //initialize reversed array
-    //SPLIT string into array
+const reverseString = function(string) {
+    
+    
     //initialize for loop, loop equal to length of split array
         //each iteration, add next letter of array to the FRONT of reversed array
     //JOIN reversed array into string
     //assign result to reversed string
+    let result = '';
+    let forwardArray = string.split('');
+    let reversedArray = [];
 
+    for (let i = 0; i < forwardArray.length; i++) {
+
+    }
+
+    return result
 };
 
 // Do not edit below this line
