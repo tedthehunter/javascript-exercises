5263bc8e869dbf2c17c67671998a4115a4207a69
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-14 20:07:21 -0700

    initialize list of removal characters from args
    this will let us use the .includes() method to parse

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) {
     //LOOK up how to handle multiple optional arguments
     //look up how to modify array in place
     //take input array and remove any instances of other arguments
 
-    //Initialize array called "removals"
-    //initialize for loop - iterate for each index in inputArray
     //for each character in array, if removals.includes(character), remove from inputArray
     //return modified array
+
+    let removalCharacters = [];
+
+    for (let i = 1; i < arguments.length; i++) {
+        removalCharacters.push(arguments[i]);
+    }
+
+    for (const item in inputArray) {
+        if (removalCharacters.includes(item)) {
+            inputArray.
+        }
+    }
+
+
 };
 
 // Do not edit below this line
