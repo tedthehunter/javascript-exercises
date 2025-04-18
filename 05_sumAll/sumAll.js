1c6aab81403a9d7dad0c65548b9a0ce99f865874
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-18 04:25:58 -0700

    initialize variables and for loop in sumAll
    leaves only more difficult logic free to write

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
     //list all nums between num1 and num2, inclusive
     //iterate through list, adding each num to result
     //return result
+    let sum = 0;
+    for (let i = num1; i = num2; i++) {
+
+    }
+    return sum;
 };
 
 // Do not edit below this line
