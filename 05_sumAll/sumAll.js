bec15309bfb5885935cb0a44706b42d176a486e3
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-18 05:21:10 -0700

    write for loop to validate inputs
    ensures inputs are positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,18 +4,37 @@ const sumAll = function(num1, num2) {
     //order args from small to large
     //plug into for loop
 
-    for (arg in arguments) {
-        if (arg % 1 != 0) {
+    for (const arg of arguments) {
+        if (typeof arg != 'number') {
+            return 'ERROR';
+        }
+        else if (arg < 0) {
+            return 'ERROR';
+        }
+        else if (arg % 1 != 0) {
             return 'ERROR';
         }
     }
+
+    let smallNum = 0;
+    let largeNum = 0;
+
+    if (num1 > num2) {
+        largeNum = num1;
+        smallNum = num2;
+    }
+    else {
+        largeNum = num2;
+        smallNum = num1;
+    }
     
     let sum = 0;
-    for (let i = num1; i <= num2; i++) {
+    for (let i = smallNum; i <= largeNum; i++) {
         sum += i;
     }
     return sum;
 };
 
+sumAll(2, 4);
 // Do not edit below this line
 module.exports = sumAll;
