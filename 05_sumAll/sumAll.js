b4f291a325ef0fa3fd68bc7923beb272a386f60f
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-18 04:24:00 -0700

    write pseudocode solving sumAll() logic
    gives baseline from which to write program

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function() {
-
+    //list all nums between num1 and num2, inclusive
+    //iterate through list, adding each num to result
+    //return result
 };
 
 // Do not edit below this line
