87b07869a502f2be5973580866194387a771e061
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-22 05:14:41 -0700

    write math expressions and return statements
    converts F to C and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,13 +7,13 @@ const convertToCelsius = function(temp) {
   return Number(((temp - 32) * (5 / 9)).toPrecision(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
   //take temp as input
   //perform some math on it
   //end with temp.toPrecision(1)
   //wrap above in Number()
   //return result
-  
+  return Number(((temp * (9 / 5)) + 32).toPrecision(1));
 };
 
 // Do not edit below this line
