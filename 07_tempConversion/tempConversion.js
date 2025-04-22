5d918cd7aaad2bd3782b59a19dcf137feffe3e14
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-22 05:20:07 -0700

    change toPrecision to toFixed in both conversions
    faulty logic, tests failed on longer or shorter inputs

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temp) {
-  return Number(((temp - 32) * (5 / 9)).toPrecision(2));
+  return Number(((temp - 32) * (5 / 9)).toFixed(1));
 };
 
 const convertToFahrenheit = function(temp) {
-  return Number(((temp * (9 / 5)) + 32).toPrecision(2));
+  return Number(((temp * (9 / 5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
