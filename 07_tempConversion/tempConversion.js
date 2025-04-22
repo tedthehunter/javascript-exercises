63466c9bd314b1fbd020737802e74f46b44825a5
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-22 05:12:35 -0700

    write pseudo code for both conversion functions
    gives a boilerplate to follow and checks logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //take temp as input
+  //perform some math on it
+  //end with temp.toPrecision(1)
+  //wrap above in Number()
+  //return result
+  return Number(((temp - 32) * (5 / 9)).toPrecision(1));
 };
 
 const convertToFahrenheit = function() {
+  //take temp as input
+  //perform some math on it
+  //end with temp.toPrecision(1)
+  //wrap above in Number()
+  //return result
+  
 };
 
 // Do not edit below this line
