9293fa03de9c70b7154e27727c5298b9440449ae
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-22 04:57:55 -0700

    write if/else statement checking remainder of input
    follows the boilerplate set by pseudo code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
     //take year as input
     //check if year is divisible by 100 AND NOT 400
         //if so - return false
         //if not check if year is divisible by 4
             //if so - return true
             //if not - return false
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0 && year % 400 != 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
