1490336f5c3c31313b1a4c01d4172835716b39c0
tedthehunter <weinheimer.raymond@gmail.com>
2025-04-22 04:53:29 -0700

    re-write pseudocode for leapYears function
    corrected faulty logic, gives a roadmap for coding

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function() {
-
+    //take year as input
+    //check if year is divisible by 100 AND NOT 400
+        //if so - return false
+        //if not check if year is divisible by 4
+            //if so - return true
+            //if not - return false
 };
 
 // Do not edit below this line
