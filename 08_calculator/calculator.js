66f0aa40135e2b5c72a1afb31daa18147c9508a0
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-14 05:17:32 -0700

    write if statement and while loop for factorial
    catches 0 case, and returns factorial for any integer

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,18 @@ const power = function(num1, num2) {
 	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  else {
+    let result = 1;
+    while (num > 0) {
+      result *= num;
+      num--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
