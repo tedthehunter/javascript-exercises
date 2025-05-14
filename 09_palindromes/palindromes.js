8ad3e270861963a0dc5ca0fe4d44a62627c28c61
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-14 05:35:55 -0700

    write pseudocode for palindrome exercise
    gives list of instructions to work with

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let modifiedString = string.replaceAll(' ', '');
+    //remove punctuation
+    //tolowercase
+    //split to array
+    //copy array and reverse
+    //return array == reverse
 };
 
 // Do not edit below this line
