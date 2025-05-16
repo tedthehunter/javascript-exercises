000ba01c36c10ec61d662720ce8b12cdc8439187
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-16 05:33:17 -0700

    set up fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 0) {
+        return 0;
+    }
+    else if (num == 1 || num == 2) {
+        return 1;
+    }
+    let counter = 2;
+    let previous = 1;
+    let result = 1;
+    do {
+        result += previous;
+        counter++;
+    } while (counter < num)
+    return result;
 };
 
 // Do not edit below this line
