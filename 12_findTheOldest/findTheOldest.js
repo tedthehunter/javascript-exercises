629a5b649cc9573a574e6dfb6905f4eb33b0054c
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-17 07:42:40 -0700

    write pseudocode for exercise
    planning to sort input array by age and return 1st

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    //SORT array
+        //sort function compares first person's death date - birth date
+            //to the second persons
+    //RETURN array[0]
 };
 
 // Do not edit below this line
