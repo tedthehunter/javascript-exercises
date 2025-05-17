a710a7d0a15cfe1107d0f5aae9db91b093c9884f
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-17 07:23:15 -0700

    write reduce function initializing empty array
    should add each book title to empty array and return

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,7 +2,9 @@ const getTheTitles = function(books) {
     //iterate over books and add titles to output array
     //reduce function
         //begin with empty array, add book.title
-    
+    return books.reduce(function(book) {
+        return book.title;
+    }, []);
 };
 
 // Do not edit below this line
