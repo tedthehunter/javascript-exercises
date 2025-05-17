4fda9f9770c49b7e990756dfdc27d50a4c51ec13
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-17 07:10:24 -0700

    remove <= from for statement, and return fibo.at()
    neg indexing does not work natively on arrays in js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -8,11 +8,11 @@ const fibonacci = function(num) {
     }
 
     let fibo = [1, 1];
-    for (let i = 2; i <= targetMember; i++) {
+    for (let i = 2; i < targetMember; i++) {
         fibo[i] = fibo[i - 1] + fibo[i - 2];
     }
-
-    return fibo[-1];
+    
+    return fibo.at(-1);
 };
 
 // Do not edit below this line
