213b4ea453fed40f2d9efeaee1dbbd129c711f83
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-17 07:02:31 -0700

    write if statement checking that input is a number
    parses strings into number types for function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,13 @@
 const fibonacci = function(num) {
-    if (num == 0) {
-        return 0;
+    let targetMember;
+    if (typeof num != 'number') {
+        targetMember = parseInt(num);
     }
-    else if (num == 1 || num == 2) {
-        return 1;
+    else {
+        targetMember = num;
     }
-    let counter = 2;
-    let previous = 1;
-    let result = 1;
-    do {
-        result += previous;
-        counter++;
-    } while (counter < num)
-    return result;
+
+    
 };
 
 // Do not edit below this line
