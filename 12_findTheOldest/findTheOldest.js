5d338b28679f818e22ebed3cc4a0df208f37ef9d
tedthehunter <weinheimer.raymond@gmail.com>
2025-05-17 08:13:36 -0700

    unskip all tests, refactor code, remove logs
    the function now returns oldest, living or dead

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,26 +1,23 @@
 const findTheOldest = function(people) {
-    
+
     const peopleSort = (firstPerson, secondPerson) => {
-        const firstPersonAge = dead(firstPerson) ?
+
+        const firstPersonAge = isDead(firstPerson) ?
             firstPerson.yearOfDeath - firstPerson.yearOfBirth :
             new Date().getFullYear() - firstPerson.yearOfBirth;
-        const secondPersonAge = dead(secondPerson) ?
+            
+        const secondPersonAge = isDead(secondPerson) ?
             secondPerson.yearOfDeath - secondPerson.yearOfBirth :
             new Date().getFullYear() - secondPerson.yearOfBirth;
-
-        //console.log(`${firstPerson.name} is ${firstPersonAge} years old.`)
-        //console.log(`${secondPerson.name} is ${secondPersonAge} years old.`)
 
         return firstPersonAge - secondPersonAge;
     }
 
-    const dead = (person) => {
-        //console.log(person.yearOfDeath);
+    const isDead = (person) => {
         return person.yearOfDeath;
     };
 
     people.sort(peopleSort);
-    console.log(people);
     return people.at(-1);
 };
 
